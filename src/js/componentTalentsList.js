0a2c918256ad067e963599d22e2836ea2369df31
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-08-16 13:25:18 +0300

    изменение классов на глобальные

--- src/js/componentTalentsList.js
+++ src/js/componentTalentsList.js
@@ -1,23 +1,25 @@
 export default function componentTalentsList() {
-  const hostElem = document.querySelector('#talents-host');
-  const linkElems = hostElem.querySelectorAll('.talents__item-link');
-  const talentsContentElem = hostElem.querySelector('.talents__content');
+  const hostElem = document.querySelector('#talents-list-host');
+  if (hostElem) {
+    const linkElems = hostElem.querySelectorAll('.talents__item-link');
+    const talentsContentElem = hostElem.querySelector('.talents__content');
 
-  linkElems.forEach((link, index) => {
-    link.onmouseenter = () => {
-      linkElems.forEach((elem, i) => {
-        if (i !== index) {
-          elem.classList.add('no-hover');
-        } else {
-          elem.classList.remove('no-hover');
-        }
+    linkElems.forEach((link, index) => {
+      link.onmouseenter = () => {
+        linkElems.forEach((elem, i) => {
+          if (i !== index) {
+            elem.classList.add('no-hover');
+          } else {
+            elem.classList.remove('no-hover');
+          }
+        })
+      }
+    })
+
+    talentsContentElem.onmouseout = () => {
+      linkElems.forEach(link => {
+        link.classList.remove('no-hover');
       })
     }
-  })
-
-  talentsContentElem.onmouseout = () => {
-    linkElems.forEach(link => {
-      link.classList.remove('no-hover');
-    })
   }
 }
