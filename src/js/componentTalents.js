7e8d934809fc93ce08022b34d194ad5220505d95
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-08-18 17:46:04 +0300

    ширина фото стр таланты

--- src/js/componentTalents.js
+++ src/js/componentTalents.js
@@ -5,7 +5,17 @@ export default function componentTalents() {
   const hostElem = document.querySelector('#talent-host');
 
   if (hostElem) {
+    const descriptionPhoto = hostElem.querySelector('.talent__description-photo');
+
     commonCatalog(hostElem);
     commonVideo(hostElem);
+
+    const setPhotoHeight = () => {
+      descriptionPhoto.style.height = `${ descriptionPhoto.clientWidth }px`;
+    }
+
+    setPhotoHeight();
+
+    window.addEventListener('resize', setPhotoHeight);
   }
 }
