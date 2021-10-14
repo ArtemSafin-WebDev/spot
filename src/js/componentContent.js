bfe0d2340f42b13c2c985c87ddc5be45e7e4c1cb
Elizaveta <eli3avetta@mail.ru>
2021-10-14 13:18:42 +0300

    save

--- src/js/componentContent.js
+++ src/js/componentContent.js
@@ -4,15 +4,19 @@ Swiper.use([Navigation, Pagination]);
 
 export default function componentContent() {
   const hostElem = document.querySelector('#content-host');
+  if (!hostElem) return;
+
   const sliderElem = hostElem.querySelector('.js-swiper-container');
   const btnPrev = hostElem.querySelector('.js-btn-prev');
   const btnNext = hostElem.querySelector('.js-btn-next');
   const btnsContent = hostElem.querySelectorAll('.js-btn-content');
+
+  const sliderTabletElem = hostElem.querySelector('.js-swiper-container-tablet');
+  const btnPrevTablet = hostElem.querySelector('.js-btn-prev-tablet');
+  const btnNextTablet = hostElem.querySelector('.js-btn-next-tablet');
   const contentElems = hostElem.querySelectorAll('.js-content');
 
-  if (!hostElem) return;
-
-  new Swiper(sliderElem, {
+  const swiperDesk = new Swiper(sliderElem, {
     slidesPerView: 1,
     lazy: true,
     keyboard: true,
@@ -20,6 +24,17 @@ export default function componentContent() {
     navigation: {
       prevEl: btnPrev,
       nextEl: btnNext
+    }
+  })
+
+  const swiperMobile = new Swiper(sliderTabletElem, {
+    slidesPerView: 1,
+    lazy: true,
+    keyboard: true,
+    speed: 400,
+    navigation: {
+      prevEl: btnPrevTablet,
+      nextEl: btnNextTablet
     }
   })
 
@@ -33,5 +48,18 @@ export default function componentContent() {
         }
       })
     }
+  })
+
+  swiperMobile.on('slideChange', () => {
+  })
+
+  swiperMobile.on('slideNextTransitionStart', () => {
+    contentElems[swiperMobile.activeIndex].classList.add('mod-show')
+    contentElems[swiperMobile.activeIndex - 1].classList.remove('mod-show')
+  })
+
+  swiperMobile.on('slidePrevTransitionStart', () => {
+    contentElems[swiperMobile.activeIndex].classList.add('mod-show')
+    contentElems[swiperMobile.activeIndex + 1].classList.remove('mod-show')
   })
 }
