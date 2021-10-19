1a3b25ca02c034ce17d23daea4ea3df2567817a5
Elizaveta <eli3avetta@mail.ru>
2021-10-19 09:06:44 +0300

    fix content page

--- src/js/componentContent.js
+++ src/js/componentContent.js
@@ -9,12 +9,10 @@ export default function componentContent() {
   const sliderElem = hostElem.querySelector('.js-swiper-container');
   const btnPrev = hostElem.querySelector('.js-btn-prev');
   const btnNext = hostElem.querySelector('.js-btn-next');
-  const btnsContent = hostElem.querySelectorAll('.js-btn-content');
 
   const sliderTabletElem = hostElem.querySelector('.js-swiper-container-tablet');
   const btnPrevTablet = hostElem.querySelector('.js-btn-prev-tablet');
   const btnNextTablet = hostElem.querySelector('.js-btn-next-tablet');
-  const contentElems = hostElem.querySelectorAll('.js-content');
 
   const swiperDesk = new Swiper(sliderElem, {
     slidesPerView: 1,
@@ -36,30 +34,5 @@ export default function componentContent() {
       prevEl: btnPrevTablet,
       nextEl: btnNextTablet
     }
-  })
-
-  btnsContent.forEach(btn => {
-    btn.onclick = () => {
-      contentElems.forEach(elem => {
-        if (elem.hasAttribute(`data-${ btn.value }`)) {
-          elem.classList.add('mod-show');
-        } else {
-          elem.classList.remove('mod-show');
-        }
-      })
-    }
-  })
-
-  swiperMobile.on('slideChange', () => {
-  })
-
-  swiperMobile.on('slideNextTransitionStart', () => {
-    contentElems[swiperMobile.activeIndex].classList.add('mod-show')
-    contentElems[swiperMobile.activeIndex - 1].classList.remove('mod-show')
-  })
-
-  swiperMobile.on('slidePrevTransitionStart', () => {
-    contentElems[swiperMobile.activeIndex].classList.add('mod-show')
-    contentElems[swiperMobile.activeIndex + 1].classList.remove('mod-show')
   })
 }
