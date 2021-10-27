210512b964800ab32bbada12fef45a797977cff4
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-10-27 11:24:30 +0300

    изменение мобильного меню в последней странице

--- src/js/componentContent.js
+++ src/js/componentContent.js
@@ -9,10 +9,12 @@ export default function componentContent() {
   const sliderElem = hostElem.querySelector('.js-swiper-container');
   const btnPrev = hostElem.querySelector('.js-btn-prev');
   const btnNext = hostElem.querySelector('.js-btn-next');
+  const mobileBtnControlMenu = hostElem.querySelector('.js-mobile-btn-control-menu');
+  const mobileMenuContainerElem = hostElem.querySelector('.js-mobile-menu-list-container');
+  const mobileMenuElem = hostElem.querySelector('.js-mobile-menu-list');
+  const contentBlockElem = hostElem.querySelector('.js-content-block');
 
-  const sliderTabletElem = hostElem.querySelector('.js-swiper-container-tablet');
-  const btnPrevTablet = hostElem.querySelector('.js-btn-prev-tablet');
-  const btnNextTablet = hostElem.querySelector('.js-btn-next-tablet');
+  let isOpenMenu = false;
 
   const swiperDesk = new Swiper(sliderElem, {
     slidesPerView: 1,
@@ -25,14 +27,38 @@ export default function componentContent() {
     }
   })
 
-  const swiperMobile = new Swiper(sliderTabletElem, {
-    slidesPerView: 1,
-    lazy: true,
-    keyboard: true,
-    speed: 400,
-    navigation: {
-      prevEl: btnPrevTablet,
-      nextEl: btnNextTablet
+  mobileBtnControlMenu.onclick = () => {
+    if (isOpenMenu) {
+      mobileBtnControlMenu.classList.remove('mod-close-btn');
+      mobileMenuElem.classList.remove('mod-show');
+      mobileMenuContainerElem.style.maxHeight = '0';
+      contentBlockElem.classList.remove('mod-hide');
+    } else {
+      mobileBtnControlMenu.classList.add('mod-close-btn');
+      mobileMenuElem.classList.add('mod-show');
+      mobileMenuContainerElem.style.maxHeight = `${ mobileMenuElem.offsetHeight }px`;
+      setTimeout(() => {
+        contentBlockElem.classList.add('mod-hide');
+      }, 400)
     }
-  })
+
+    isOpenMenu = !isOpenMenu;
+  }
+
+  // для старого мобильного слайдера
+
+  // const sliderTabletElem = hostElem.querySelector('.js-swiper-container-tablet');
+  // const btnPrevTablet = hostElem.querySelector('.js-btn-prev-tablet');
+  // const btnNextTablet = hostElem.querySelector('.js-btn-next-tablet');
+
+  // const swiperMobile = new Swiper(sliderTabletElem, {
+  //   slidesPerView: 1,
+  //   lazy: true,
+  //   keyboard: true,
+  //   speed: 400,
+  //   navigation: {
+  //     prevEl: btnPrevTablet,
+  //     nextEl: btnNextTablet
+  //   }
+  // })
 }
