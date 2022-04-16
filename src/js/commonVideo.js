d2ac9921bfabc8f233ba86b2fdab8325df30147e
Artem Safin <artemsafin.webdev@gmail.com>
2022-04-17 02:12:28 +0300

    Оптимизация

--- src/js/commonVideo.js
+++ src/js/commonVideo.js
@@ -1,28 +1,62 @@
 import Plyr from 'plyr';
 
 export default function commonVideo(hostElem) {
-  const videosElems = hostElem.querySelectorAll('.gl-catalog__card-video');
+    const videosElems = hostElem.querySelectorAll('.gl-catalog__card-video');
 
-  videosElems.forEach(elem => {
-    const parent = elem.parentElement;
-    const plyr = new Plyr(elem, {
-      controls: [],
-      loadSprite: false,
-      volume: 0
+    videosElems.forEach(elem => {
+        const parent = elem.parentElement.parentElement;
+        const wrapper = elem.parentElement;
+        const loader = parent.querySelector('.gl-catalog__card-video-loader')
+
+        let plyr = null;
+
+        let ready = false;
+
+        parent.onmouseenter = () => {
+            console.log('Mouseentered');
+            loader.classList.add('active');
+            if (!plyr) {
+                plyr = new Plyr(parent.querySelector('.gl-catalog__card-video'), {
+                    controls: [],
+                    loadSprite: false,
+                    volume: 0
+                });
+
+                plyr.muted = true;
+            }
+
+            const play = () => {
+                const playPromise = plyr.play();
+                if (playPromise) {
+                    playPromise.then(() => {
+                        wrapper.classList.add('active');
+                        loader.classList.remove('active');
+                    });
+                }
+            };
+            plyr.on('ready', event => {
+                play();
+                ready = true;
+            });
+
+            if (ready) {
+                play();
+            }
+        };
+
+        parent.onmouseleave = () => {
+            if (!plyr) return;
+
+            const onTransitionEnd = () => {
+                plyr.destroy();
+                plyr = null;
+                wrapper.removeEventListener('transitionend', onTransitionEnd);
+                
+            };
+            plyr.stop();
+            wrapper.addEventListener('transitionend', onTransitionEnd);
+            wrapper.classList.remove('active');
+            loader.classList.remove('active');
+        };
     });
-
-    plyr.muted = true;
-
-    parent.onmouseenter = () => {
-      const playPromise = plyr.play();
-      if (playPromise) {
-        playPromise.then(() => {
-        })
-      }
-    }
-
-    parent.onmouseleave = () => {
-      plyr.stop();
-    };
-  });
 }
